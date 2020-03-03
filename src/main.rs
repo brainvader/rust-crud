824eb6bde54ee2570bb11d06e57b387934f916ad
BrainVader <brainvade@gmail.com>
2020-03-03 22:05:54 +0900

    Add default behaviour in not-found case

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
 use std::io::Result;
 
@@ -11,7 +11,11 @@ async fn hello_world() -> impl Responder {
 async fn main() -> Result<()> {
     let url = "127.0.0.1:8088";
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().service(hello_world));
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+            .default_service(web::to(|| HttpResponse::NotFound()))
+    });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
