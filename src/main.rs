20b0821d45d14d9da150eaf16c1d2e7a72800bb5
BrainVader <brainvade@gmail.com>
2020-03-03 21:50:40 +0900

    Use attributes macro to define route

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
 use std::io::Result;
 
+#[get("/hello")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello World!")
 }
@@ -10,7 +11,7 @@ async fn index() -> impl Responder {
 async fn main() -> Result<()> {
     let url = "127.0.0.1:8088";
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().route("/", web::get().to(index)));
+    let mut server = HttpServer::new(|| App::new().service(index));
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
