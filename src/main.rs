5ba8f8f60bb9a9b8eb6ed374dfa870dee16083f2
BrainVader <brainvade@gmail.com>
2020-03-03 21:38:14 +0900

    Add support for watch mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 use std::io::Result;
 
 async fn index() -> impl Responder {
@@ -8,8 +9,14 @@ async fn index() -> impl Responder {
 #[actix_rt::main]
 async fn main() -> Result<()> {
     let url = "127.0.0.1:8088";
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind(url)?
-        .run()
-        .await
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().route("/", web::get().to(index)));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind(url)?
+    };
+
+    server.run().await
 }
