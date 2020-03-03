8a996d6df9be358a585318c1cb9cd880938cb277
BrainVader <brainvade@gmail.com>
2020-03-03 19:50:18 +0900

    Add actix http server with application

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io::Result;
 
 async fn index() -> impl Responder {
     "Hello World!"
 }
 
 #[actix_rt::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let url = "127.0.0.1:8088";
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(url)?
+        .run()
+        .await
 }
