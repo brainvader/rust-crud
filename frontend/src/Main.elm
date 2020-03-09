5cc82fab4c12124acd9b8b3d73f1bc8405c21ad5
BrainVader <brainvade@gmail.com>
2020-03-09 20:32:38 +0900

    Add Model

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,11 +1,24 @@
 module Main exposing (main)
 
+import Browser.Navigation as Nav
 import Html exposing (Html, div, text)
 
 
 main : Html msg
 main =
     view
+
+
+type alias Model =
+    { key : Nav.Key
+    , curentPage : Page
+    }
+
+
+type Page
+    = NotFound
+    | Top
+    | About
 
 
 view : Html msg
