289710f8731f49c440f206ea85b9597d650f8e39
BrainVader <brainvade@gmail.com>
2020-03-13 21:51:14 +0900

    Add route to return custom object as JSON

--- src/example/hello.rs
+++ src/example/hello.rs
@@ -1,10 +1,34 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, web, Error, HttpRequest, HttpResponse, Responder};
+use futures::future::{ready, Ready};
+use serde::Serialize;
 
 #[get("/hello")]
 async fn hello_world() -> impl Responder {
     HttpResponse::Ok().body("Hello World!")
 }
 
+#[derive(Serialize)]
+struct MyObj {
+    name: &'static str,
+}
+
+impl Responder for MyObj {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let mime_type = "application/json";
+        let body = serde_json::to_string(&self).unwrap();
+        let response = HttpResponse::Ok().content_type(mime_type).body(body);
+        ready(Ok(response))
+    }
+}
+
+#[get("/my-obj")]
+async fn my_obj() -> impl Responder {
+    MyObj { name: "user" }
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(hello_world);
+    cfg.service(my_obj).service(hello_world);
 }
