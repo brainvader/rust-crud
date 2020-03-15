97017f3440a5be6e14ab97ca5f38d7b7a3edecd6
BrainVader <brainvade@gmail.com>
2020-03-15 17:18:27 +0900

    Add handler for 404 page-not-found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_files as fs;
+use actix_web::http::StatusCode;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use listenfd::ListenFd;
 use std::io::Result;
@@ -6,6 +8,10 @@ mod example;
 // implicit root module
 use crate::example::hello;
 
+async fn page_404() -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
+}
+
 #[actix_rt::main]
 async fn main() -> Result<()> {
     let url = "127.0.0.1:8088";
@@ -13,7 +19,7 @@ async fn main() -> Result<()> {
     let mut server = HttpServer::new(|| {
         App::new()
             .service(web::scope("/example").configure(hello::config))
-            .default_service(web::to(|| HttpResponse::NotFound()))
+            .default_service(web::to(page_404))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
