c661d34e30a01a12f9b37dbd669221ef6bdb651a
BrainVader <brainvade@gmail.com>
2020-03-15 19:44:28 +0900

    Add handler to return json reading from file

--- src/example/hello.rs
+++ src/example/hello.rs
@@ -1,12 +1,39 @@
-use actix_web::{get, web, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::{get, web, Error, HttpRequest, HttpResponse, Responder, Result};
 use futures::future::{ready, Ready};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 
 #[get("/hello")]
 async fn hello_world() -> impl Responder {
     let mut response_builder = HttpResponse::Ok();
     let body = "Hello World!";
     response_builder.body(body)
+}
+
+#[derive(Serialize, Deserialize)]
+struct Cell {
+    kind: String,
+    content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Quiz {
+    question: Vec<Cell>,
+    answer: Vec<Cell>,
+}
+
+#[get("what-is-minq")]
+async fn what_is_minq() -> Result<HttpResponse> {
+    let mut response_builder = HttpResponse::Ok();
+    let path_to_file = Path::new("./quiz/what-is-minq.json");
+    let file = File::open(path_to_file)?;
+    let reader = BufReader::new(file);
+    let quiz: Quiz = serde_json::from_reader(reader)?;
+    let mime_type = "application/json";
+    let response = response_builder.content_type(mime_type).json(quiz);
+    Ok(response)
 }
 
 #[derive(Serialize)]
@@ -33,5 +60,7 @@ async fn my_obj() -> impl Responder {
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(my_obj).service(hello_world);
+    cfg.service(my_obj)
+        .service(hello_world)
+        .service(what_is_minq);
 }
