fe9b5bf1bdbb2a83efebb0363f4025e501e828dc
BrainVader <brainvade@gmail.com>
2020-03-20 21:20:03 +0900

    Add buildErrorMessage function

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -123,6 +123,25 @@ update msg preModel =
                     ( preModel, Cmd.none )
 
 
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+
 view : Model -> Browser.Document Msg
 view model =
     case model.currentPage of
